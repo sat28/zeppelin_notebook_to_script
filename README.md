70d57c479c987b1b7711efd15bb5bb3df6380575
Shaleen <anand.shaleen@gmail.com>
2018-04-20 11:47:08 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # zeppelin_notebook_to_script
 Converting a zeppelin notebook in single programming language to respective script
+
+# Execution command
+zeppelin_to_script.py input_file_name output_file_name
+
+output_file_name - can have the required extension.
---
+++ zeppelin_to_script.py
@@ -0,0 +1,18 @@
+import sys
+input_file_name = sys.argv[1]
+output_file_name = sys.argv[2]
+
+import json
+import codecs
+notebook = json.load(codecs.open(input_file_name, 'r', 'utf-8-sig'))
+
+script = []
+for i in notebook["paragraphs"]:
+    para = i["text"].splitlines()[1:]
+    if len(para) > 0:
+        para = '\n'.join(para)
+        script.append(para)
+script_f = '\n\n'.join(script)
+
+with open(output_file_name, 'w') as the_file:
+    the_file.write(script_f)
