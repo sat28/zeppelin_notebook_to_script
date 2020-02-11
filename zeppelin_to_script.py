38565724330bd4616ba6577caddae35dc4ee5666
WingCode <smallstar1234@gmail.com>
2020-02-11 13:10:05 +0530

    If cell is empty, text field won't be present
    Added check whether text field is present

--- zeppelin_to_script.py
+++ zeppelin_to_script.py
@@ -8,10 +8,11 @@ notebook = json.load(codecs.open(input_file_name, 'r', 'utf-8-sig'))
 
 script = []
 for i in notebook["paragraphs"]:
-    para = i["text"].splitlines()[1:]
-    if len(para) > 0:
-        para = '\n'.join(para)
-        script.append(para)
+    if "text" in i:
+        para = i["text"].splitlines()[1:]
+        if len(para) > 0:
+            para = '\n'.join(para)
+            script.append(para)
 script_f = '\n\n'.join(script)
 
 with open(output_file_name, 'w') as the_file:
